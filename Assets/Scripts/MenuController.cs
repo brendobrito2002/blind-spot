79418e90717cd58e9b88aa9ad1bc7e4da4eebe02
brendobrito2002 <brendoplayer@gmail.com>
2026-08-29 15:32:12 -0300

    feat: sistema de pausa e menu de pausa

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject menuCanvas;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        menuCanvas.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            menuCanvas.SetActive(!menuCanvas.activeSelf);
+            PauseController.SetPause(menuCanvas.activeSelf);
+        }
+
+    }
+
+    public void Continue()
+    {
+        menuCanvas.SetActive(false);
+        PauseController.SetPause(false);
+    }
+
+    public void RestartFase()
+    {
+        menuCanvas.SetActive(false);
+        PauseController.SetPause(false);
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().name);
+    }
+
+    public void ReturnTitleScreen()
+    {
+        SceneManager.LoadScene("TitleScreen");
+    }
+}
